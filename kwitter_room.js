275ea94c1eebc3be614f201c1911343c682a7b27
SanchitaSubramaniam <86506500+SanchitaSubramaniam@users.noreply.github.com>
2022-01-29 10:57:01 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCFzxGnUYN75ghB_0fKGYybHVH59MIpioU",
+    authDomain: "project-93---97.firebaseapp.com",
+    projectId: "project-93---97",
+    storageBucket: "project-93---97.appspot.com",
+    messagingSenderId: "964995010574",
+    appId: "1:964995010574:web:957e59a82395e350e1ec5f"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
